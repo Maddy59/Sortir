bd90781d99dfa105bfdd2fd4e19c8428d24f33a7
nandoksman <fernandooksman@gmail.com>
2021-04-14 08:57:55 -0300

    ajout unicité pseudo dans la classe User

--- sortir/src/Entity/User.php
+++ sortir/src/Entity/User.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
@@ -63,7 +64,8 @@ class User implements UserInterface
     private $actif;
 
     /**
-     * @ORM\Column(type="string", length=30)
+     * @ORM\Column(type="string", length=30, unique=true)
+     * @Assert\Unique
      */
     private $pseudo;
 
